1a8a640bb02f5c11c61eae0fed9d98149c676e2b
Richard Kasperowski <r@kasperowski.com>
2022-10-16 18:41:29 -0400

    Initial flask UI

--- flask-app.py
+++ flask-app.py
@@ -7,10 +7,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index() -> str:
-    roman: str = str(request.form.get("roman"))
-    arabic: int = 0
-    if len(roman) > 0:
+    print(request)
+    print(request.form)
+    print(request.args)
+    roman: str = str(request.args.get("roman"))
+    arabic: int = -1
+    if roman != "None":
         arabic = Roman.convert(roman)
+    print('roman: ' + roman + ', arabic: ' + str(arabic))
     return render_template("index.html", roman=roman, arabic=arabic)
 
 
